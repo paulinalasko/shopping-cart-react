6e2551ff6720a12f510191800476d1fe1da30083
Paulina Laskowska <paulinal@hawaii.edu>
2023-09-22 17:06:03 -0600

    finetuned media query

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,7 +9,7 @@ const NavBar = (props) => {
             <h1 className='logo'>higher ground</h1> 
             <NavLink className='link' exact to='/shopping-cart-react'>Home</NavLink>
             <NavLink className='link' to='/shopping-cart-react/newarrivals'>Products</NavLink>
-            <div className="dropdown">
+            <div className="dropdown" style={{ display: 'flex', alignItems: 'center' }}>
                 <NavLink className='link dropdown-toggle' to='/shopping-cart-react/cart'>
                 <LiaShoppingCartSolid style={{ marginRight: 5 }}/>Cart</NavLink>
                 <badge style={{ padding: 10, borderRadius: 10, background: "white" }}>{cartItems.length}</badge>
--- src/components/Product.js
+++ src/components/Product.js
@@ -8,7 +8,7 @@ export default function Product(props) {
           <div className='product'>
               <img className={product.css} src={product.imagePath} alt={product.alt}></img>
               <p className='product-description'>{product.name} <br /> ${product.price}</p>
-              <button onClick={() => onAdd(product)} className="click-btn standard-btn">Add to Cart</button>
+              <button onClick={() => onAdd(product)} onTouchStart={() => onAdd(product)} className="click-btn standard-btn">Add to Cart</button>
             </div>
           </div>
     </div>
